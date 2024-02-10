519dccf8fa046b715a6f8df13a7347e7e1ebacf0
Ankit <ankitabhishek100@gmail.com>
2024-02-11 00:46:12 +0530

    modified yml location

--- kafka-consumer-wikimedia/src/main/java/com/ankit/springboot/SpringConsumerApplication.java
+++ kafka-consumer-wikimedia/src/main/java/com/ankit/springboot/SpringConsumerApplication.java
@@ -1,11 +1,13 @@
 package com.ankit.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class SpringConsumerApplication
 {
     public static void main( String[] args )
