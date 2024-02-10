0a463044546a578ad50057df1d3f26e15dcab800
Ankit <ankitabhishek100@gmail.com>
2024-02-11 00:29:56 +0530

    added consumer project

---
+++ kafka-consumer-wikimedia/src/main/java/com/ankit/springboot/SpringConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.ankit.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpringConsumerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringConsumerApplication.class);
+    }
+}
